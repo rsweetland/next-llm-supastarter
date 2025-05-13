1614b2b05f093c4f8daab802cdddd4a58378b391
Reilly Sweetland <esweetland@gmail.com>
2025-05-13 09:33:29 -0700

    test: fix snapshot test example

--- __tests__/snapshot.test.tsx
+++ __tests__/snapshot.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import { it, expect } from 'vitest';
 
 it("renders homepage unchanged", () => {
-  // const { container } = render(<Index />);
-  // expect(container).toMatchSnapshot();
+  const { container } = render(<Index />);
+  expect(container).toMatchSnapshot();
   expect(true).toBe(true);
 });
